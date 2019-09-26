625268bf32f0d97a5b429a9bc74ddc4d68b9d5e8
Thomas George <t6george@uwaterloo.ca>
2019-09-25 20:50:05 -0400

    types

--- kernel.cpp
+++ kernel.cpp
@@ -1,10 +1,12 @@
+#include "types.h"
+
 void printf(const char *s) {
-	unsigned short* stdout = (unsigned short*) 0xb8000;
+	uint16_t* stdout = (uint16_t*) 0xb8000;
 	for (int i = 0; *(s+i) != '\0'; ++i)
 		*stdout = (*stdout & 0xFF00) | *(s+i);
 }
 
-extern "C" void kernelEntry(void *multibootStruct, unsigned int magicNum) {
-	printf ("hello from mOS\n");
+extern "C" void kernelEntry(void *multibootStruct, uint32_t magicNum) {
+	printf ("hello moss\n");
 	while(1);
 }
