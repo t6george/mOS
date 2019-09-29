116c965c094f805093873eeda72c5982d6305343
Thomas George <t6george@uwaterloo.ca>
2019-09-28 21:04:25 -0400

    console class layout

--- types.h
+++ types.h
@@ -1,16 +1,16 @@
 #ifndef TYPES_H
 #define TYPES_H
 
-typedef char int8_t;
-typedef unsigned char uint8_t;
+typedef char i8;
+typedef unsigned char u8;
 
-typedef short int16_t;
-typedef unsigned short uint16_t;
+typedef short i16;
+typedef unsigned short u16;
 
-typedef int int32_t;
-typedef unsigned int uint32_t;
+typedef int i32;
+typedef unsigned int u32;
 
-typedef long long int int64_t;
-typedef unsigned long long int uint64_t;
+typedef long long int i64;
+typedef unsigned long long int u64;
 
 #endif
