1ec7e9a86f41e2d8766801ad7133bd17036cac37
Thomas George <t6george@edu.uwaterloo.ca>
2019-09-29 13:08:44 -0400

    added Makefile

--- loader.s
+++ loader.s
@@ -3,6 +3,7 @@ global loader
 MAGIC_NUMBER equ 0x1BADB002
 FLAGS	     equ 0x0
 CHECKSUM     equ -MAGIC_NUMBER
+STACK_SIZE   equ 0x1000
 
 section .text:
 align 4
@@ -11,7 +12,16 @@ align 4
 	dd CHECKSUM
 
 loader:
+	mov esp, kernel_stack + STACK_SIZE
+
+	; using cdecl calling convention
+	
 	mov eax, 0xCAFEBABE
 
 .loop:
 	jmp .loop
+
+section .bss
+align 4
+	kernel_stack:
+		resb STACK_SIZE
